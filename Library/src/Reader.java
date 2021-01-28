5ec70c519568d0cf13656ac23da528b4b361bfd3
Szymon Fialkowski <szymon.fialkowski1@gmail.com>
2021-01-28 13:02:00 +0100

    Szymon

--- Library/src/Reader.java
+++ Library/src/Reader.java
@@ -1,32 +1,57 @@
-import java.io.Serializable;
 
-public class Reader implements Serializable {
+public class Reader  {
+
     private String name;
     private String surname;
     private int age;
     private String address;
     private String banned;
-    public Reader(String name, String surname, int age, String address, String banned){
-        this.name=name;
-        this.surname=surname;
-        this.age=age;
-        this.address=address;
-        this.banned=banned;
+
+    public Reader(String name, String surname, int age, String address, String banned) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+            this.banned = banned;
     }
-    public String getName(){
-        return name;
+    // Gettery
+    public String getName() {
+            return name;
     }
-    public String getSurname(){
-        return surname;
+
+    public String getSurname() {
+            return surname;
     }
-    public int getAge(){
-        return age;
+
+    public int getAge() {
+            return age;
     }
-    public String getAddress(){
-        return address;
+
+    public String getAddress() {
+            return address;
     }
-    public String getBanned(){
-        return banned;
+
+    public String getBanned() {
+            return banned;
+    }
+    // Settery
+    public void setName(String name) {
+            this.name = name;
     }
     
+    public void setSurname(String surname) {
+            this.surname = surname;
+    }
+    
+    public void setAge(int age){
+            this.age = age;
+    }
+    
+    public void setAddress(String address){
+            this.address = address;
+    }
+    
+    public void setBanned(String banned){
+            this.banned = banned;
+    }
 }
