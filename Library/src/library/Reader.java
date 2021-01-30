dc61152815f2f10993f7304c7ef2bc2e1c98101e
Szymon Fialkowski <szymon.fialkowski1@gmail.com>
2021-01-30 12:40:22 +0100

    Szymon

--- Library/src/library/Reader.java
+++ Library/src/library/Reader.java
@@ -1,20 +1,28 @@
 package library;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Reader  {
 
+public class Reader implements Serializable {
+private static final long serialVersionUID = -2908137848364231871L;
+    
+    
     private String name;
     private String surname;
     private int age;
     private String address;
     private String banned;
+    private String test;
 
-    public Reader(String name, String surname, int age, String address, String banned) {
+    public Reader(String name, String surname, int age, String address, String banned, String test) {
             this.name = name;
             this.surname = surname;
             this.age = age;
             this.address = address;
             this.banned = banned;
+            this.test = test;
     }
     // Gettery
     public String getName() {
@@ -56,4 +64,20 @@ public class Reader  {
     public void setBanned(String banned){
             this.banned = banned;
     }
+    
+    
+    // toString.
+     public String toString() 
+    {
+        return 
+             "---------------------------Czytelnik---------------------------" +
+                "\nImiê: " + name + 
+                "\nNazwisko: "+ surname + 
+                "\nWiek: "+ age + 
+                "\nAdres: " + address +
+                "\nZablokowany: " + banned +
+                "\nWypo¿yczona ksi¹¿ka: " + test +"\n\n";
+    }
+
+
 }
