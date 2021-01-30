429350d39681b03b49533ee09fd857e0f4c397e6
Szymon Fialkowski <szymon.fialkowski1@gmail.com>
2021-01-30 21:08:11 +0100

    Szymon

--- Library/src/library/Reader.java
+++ Library/src/library/Reader.java
@@ -7,22 +7,27 @@ import java.util.List;
 
 public class Reader implements Serializable {
 private static final long serialVersionUID = -2908137848364231871L;
-    
-    
+   
+    List<Book> wypozyczone = new ArrayList<Book>();
     private String name;
     private String surname;
     private int age;
     private String address;
     private String banned;
     private String test;
+    private long PIN;
 
-    public Reader(String name, String surname, int age, String address, String banned, String test) {
+    public Reader(){
+    }
+    
+    public Reader(String name, String surname, int age, String address, String banned, String test, long PIN) {
             this.name = name;
             this.surname = surname;
             this.age = age;
             this.address = address;
             this.banned = banned;
             this.test = test;
+            this.PIN = PIN;
     }
     // Gettery
     public String getName() {
@@ -43,6 +48,10 @@ private static final long serialVersionUID = -2908137848364231871L;
 
     public String getBanned() {
             return banned;
+    }
+    
+     public long getPIN() {
+            return PIN;
     }
     // Settery
     public void setName(String name) {
@@ -65,6 +74,13 @@ private static final long serialVersionUID = -2908137848364231871L;
             this.banned = banned;
     }
     
+    public void setPIN(long PIN){
+            this.PIN = PIN;
+    }
+    
+    public void wypozycz(Book b) {
+        wypozyczone.add(b);
+        }
     
     // toString.
      public String toString() 
@@ -76,8 +92,28 @@ private static final long serialVersionUID = -2908137848364231871L;
                 "\nWiek: "+ age + 
                 "\nAdres: " + address +
                 "\nZablokowany: " + banned +
-                "\nWypo¿yczona ksi¹¿ka: " + test +"\n\n";
+                "\nWypo¿yczona ksi¹¿ka: " + test +
+                "\nPesel: " + PIN +"\n\n";
     }
-
-
+     
+      public Reader znajdzUzytkownika(List<Reader> readers, long PIN) {
+        for(Reader reader : readers){
+              if (reader.PIN == PIN) {
+                return reader;
+            }
+        }return null;
 }
+      
+       public void printWypozyczone() 
+    {
+        for (int i = 0; i < wypozyczone.size(); i++) 
+        {
+          Book b = wypozyczone.get(i);
+            System.out.println(b.toString());
+        }
+    }
+}
+     
+
+
+
