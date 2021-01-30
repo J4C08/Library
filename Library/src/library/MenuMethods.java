6b87bc3285b0fb41d27b6769397fad94879d25bc
Szymon Fialkowski <szymon.fialkowski1@gmail.com>
2021-01-30 10:39:13 +0100

    Szymon

--- Library/src/library/MenuMethods.java
+++ Library/src/library/MenuMethods.java
@@ -5,14 +5,20 @@ package library;
 //  Name:           Imports. 
 //  Description:    To allow the use of different Java classes.
 //---------------------------------------------------------------------------------------
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 //---------------------------------------------------------------------------------------
 public class MenuMethods {
-
-
+    
+    
     private static Scanner keyboard = new Scanner(System.in);
 //---------------------------------------------------------------------------------------
-//  Methods for the Company Application menu.
+//  Methods for the Library Application menu.
 //---------------------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------------------
@@ -49,22 +55,22 @@ public class MenuMethods {
 
 //---------------------------------------------------------------------------------------
 //  Name:        userInput
-//  Description: This method is used in the MainApp to give the user capability to enter
+//  Description: This method is used in the Main to give the user capability to enter
 //               the details when adding details of an employee into the store.
 //---------------------------------------------------------------------------------------
     public static Book userInput() 
     {
         String temp = keyboard.nextLine();
         Book s = null;
-        System.out.println("Please enter the Student Name:");
+        System.out.println("Please enter the name book:");
         String nameBook = keyboard.nextLine();
-        System.out.println("Please enter the Student ID:");
+        System.out.println("Please enter the author:");
         String author = keyboard.nextLine();
-        System.out.println("Please enter the Student E-mail address:");
+        System.out.println("Please enter the publisher:");
         String publisher = keyboard.nextLine();
-        System.out.println("Please enter the Student telephone number:");
+        System.out.println("Please enter the publisher year:");
         int publishingYear = keyboard.nextInt();
-        System.out.println("Please enter the Student telephone number:");
+        System.out.println("Please enter the numer of books:");
         int numberOfBooks = keyboard.nextInt();
         
         return s = new Book(nameBook, author, publisher, publishingYear, numberOfBooks );
@@ -75,99 +81,21 @@ public class MenuMethods {
 //  Name:        userInputByName.
 //  Description: This method is used in the MainApp to give the user capability to search by name.
 //---------------------------------------------------------------------------------------
-    public static Book userInputByBookName() 
+ public static String userInputByBookName() 
     {
         // String temp is for some reason needed. If it is not included
         // The code will not execute properly.
         String temp = keyboard.nextLine();
         Book s = null;
         System.out.println("Please enter the Student Name:");
-        String bookName = keyboard.nextLine();
+        String nameBook = keyboard.nextLine();
 
-        return s = new Book(bookName);
-
-    }
-
-//---------------------------------------------------------------------------------------
-//  Name:        userInputByEmail
-//  Description: This method is used in the MainApp to give the user capability to search by email.
-//---------------------------------------------------------------------------------------
-    public static String userInputByAuthor() 
-    {
-        // String temp is for some reason needed. If it is not included
-        // The code will not execute properly.
-        String temp = keyboard.nextLine();
-        Book s = null;
-        System.out.println("Please enter the StudentEmail:");
-        String author = keyboard.nextLine();
-        // This can use the employeeName's constructor because java accepts the
-        // parameters instead
-        // of the name's.
-        return author;
+        return nameBook;
 
     }
-//---------------------------------------------------------------------------------------
  
-//---------------------------------------------------------------------------------------
-//  Name:        userInputByEmail
-//  Description: This method is used in the MainApp to give the user capability to search by email.
-//---------------------------------------------------------------------------------------
-    public static String userInputByPublisher() 
-    {
-        // String temp is for some reason needed. If it is not included
-        // The code will not execute properly.
-        String temp = keyboard.nextLine();
-        Book s = null;
-        System.out.println("Please enter the StudentEmail:");
-        String publisher = keyboard.nextLine();
-        // This can use the employeeName's constructor because java accepts the
-        // parameters instead
-        // of the name's.
-        return publisher;
 
-    }
-//---------------------------------------------------------------------------------------
-        
-//---------------------------------------------------------------------------------------
-//  Name:        userInputByEmail
-//  Description: This method is used in the MainApp to give the user capability to search by email.
-//---------------------------------------------------------------------------------------
-    public static String userInputByPublishingYear() 
-    {
-        // String temp is for some reason needed. If it is not included
-        // The code will not execute properly.
-        String temp = keyboard.nextLine();
-        Book s = null;
-        System.out.println("Please enter the StudentEmail:");
-        String publishingYear = keyboard.nextLine();
-        // This can use the employeeName's constructor because java accepts the
-        // parameters instead
-        // of the name's.
-        return publishingYear;
-
-    }
-//---------------------------------------------------------------------------------------
-        
-//---------------------------------------------------------------------------------------
-//  Name:        userInputByEmail
-//  Description: This method is used in the MainApp to give the user capability to search by email.
-//---------------------------------------------------------------------------------------
-    public static String userInputByNumberOfBooks() 
-    {
-        // String temp is for some reason needed. If it is not included
-        // The code will not execute properly.
-        String temp = keyboard.nextLine();
-        Book s = null;
-        System.out.println("Please enter the StudentEmail:");
-        String numberOfBooks = keyboard.nextLine();
-        // This can use the employeeName's constructor because java accepts the
-        // parameters instead
-        // of the name's.
-        return numberOfBooks;
-
-    }
-//---------------------------------------------------------------------------------------
+  }
     
-    
-}
+
 
